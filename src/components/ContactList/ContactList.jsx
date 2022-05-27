96570d3f0adc297c920916d896eb6c8bfb8ef4cc
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-05-27 20:05:16 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,8 +1,8 @@
-// import { List,ButtonDelete, ItemList, ContactName, ContactNumber } from './ContactList.styled';
+import { List,ButtonDelete, ItemList, ContactName, ContactNumber } from './ContactList.styled';
 // import { useGetContactsQuery, useDeleteContactMutation } from '../../redux/contactsSlice';
 // import { useSelector } from 'react-redux';
-// import { useMemo } from 'react';
-// import { Loader } from 'components/Loader/Loader';
+// // import { useMemo } from 'react';
+import { Loader } from 'components/Loader/Loader';
 
 
 
@@ -10,6 +10,8 @@ const ContactList = () => {
   // const { data = [], isLoading } = useGetContactsQuery();
   // const [deleteContacts] = useDeleteContactMutation();
   // const filter = useSelector(state => state.filter.value);
+  const data = [];
+  const isLoading = false;
 
   // const visibleContacts = useMemo(
   //   () => {
@@ -28,27 +30,28 @@ const ContactList = () => {
     // },[data, filter])
   // const visibleContacts = [];
 
-  // const deleteContact = id => {
-  //   deleteContacts(id);
-  // };
+  const deleteContact = id => {
+    // deleteContacts(id);
+    console.log(id);
+  };
 
   return (
-    <p>list</p>
-  //   <List>
-  //   {isLoading && <Loader/>}
-  //   {visibleContacts.length === 0 && <h3>...oops has no contacts :(</h3>}
-  //   {visibleContacts.map(({ id, name, number, phone }) => (
-  //     <ItemList key={id}>
-  //       <p>
-  //         <ContactName>{name}:</ContactName>
-  //         <ContactNumber href={`tel:${number??phone}`}>{number??phone}</ContactNumber>
-  //       </p>
-  //       <ButtonDelete type="button" onClick={() => deleteContact(id)}>
-  //         Delete
-  //       </ButtonDelete>
-  //     </ItemList>
-  //   ))}
-  // </List>
+    // <p>list</p>
+    <List>
+    {isLoading && <Loader/>}
+    {data.length === 0 && <h3>...oops has no contacts :(</h3>}
+    {data.map(({ id, name, number, phone }) => (
+      <ItemList key={id}>
+        <p>
+          <ContactName>{name}:</ContactName>
+          <ContactNumber href={`tel:${number??phone}`}>{number??phone}</ContactNumber>
+        </p>
+        <ButtonDelete type="button" onClick={() => deleteContact(id)}>
+          Delete
+        </ButtonDelete>
+      </ItemList>
+    ))}
+  </List>
 );
 }
 
