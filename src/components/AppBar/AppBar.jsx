bfde0cb76bc9d25f424aa0fdf4fa2545214f8a8f
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-05-28 16:25:05 +0300

    add user menu

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,9 +1,15 @@
 import { Link, Header, Logo, Navigation, LogoIcon, Box } from './AppBar.styled';
 import { NavLink } from 'react-router-dom';
 import logo from '../../images/long-film.png'
+import UserMenu from '../UserMenu/UserMenu';
 import { refs } from "../../services/refs";
+import { useSelector } from 'react-redux';
+import authSelectors from '../../redux/auth/authSelectors'
 
-export  function AppBar() {
+export function AppBar() {
+    const isLoggedIn = useSelector(authSelectors.getIsLoggedIn)
+    console.log(isLoggedIn);
+
     return (
         <Header>
             <Navigation>
@@ -14,10 +20,18 @@ export  function AppBar() {
                     <Link to={refs.layout}>Home</Link>
                     <Link to={refs.contactsPage}>Contacts</Link>
                 </Box>
+                {isLoggedIn ?
+                    <Box>
+                    <UserMenu></UserMenu>
+                </Box>
+                        :
                 <Box>
                     <Link to={refs.loginPage}>Login</Link>
                     <Link to={refs.registerPage}>Registration</Link>
                 </Box>
+                    
+                }
+                
             </Navigation>
         </Header>
     )
